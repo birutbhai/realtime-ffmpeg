d4dbff8c156975e8cc575021d03e9a31b1bf86d9
Yaswanth Yadlapalli <y.yaswanth89@gmail.com>
2019-11-16 12:26:05 -0600

    update base_task
    Signed-off-by: Yaswanth Yadlapalli <y.yaswanth89@gmail.com>

--- base_task2.c
+++ base_task2.c
@@ -10,9 +10,16 @@
  * system call blocks. To be on the safe side, only use I/O for debugging
  * purposes and from non-real-time sections.
  */
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/imgutils.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_thread.h>
 
 /* Second, we include the LITMUS^RT user space library header.
  * This header, part of liblitmus, provides the user space API of
@@ -20,6 +27,8 @@
  */
 #include "litmus.h"
 
+
+void printHelpMenu();
 /* Next, we define period and execution cost to be constant. 
  * These are only constants for convenience in this example, they can be
  * determined at run time, e.g., from command line parameters.
@@ -61,8 +70,27 @@ int job(void);
  * LITMUS^RT real-time task. In a real program, all the return values should be 
  * checked for errors.
  */
+
+AVFormatContext * pFormatCtx;
+AVCodecContext * pCodecCtx;
+int videoStream;
+AVCodec * pCodec;
+AVFrame * pFrame;
+AVFrame * pict;
+SDL_Renderer * renderer;
+SDL_Texture * texture;
+AVPacket * pPacket;
+struct SwsContext * sws_ctx;
+
 int main(int argc, char** argv)
 {
+	if ( !(argc > 2) ){
+        printHelpMenu();
+        return -1;
+    }
+
+    int ret = -1;
+
 	int do_exit;
 	struct rt_task param;
 
@@ -87,14 +115,174 @@ int main(int argc, char** argv)
 	/*****
 	 * 1) Command line paramter parsing would be done here.
 	 */
-
-
-
+	pFormatCtx = NULL;
+	pCodecCtx = NULL;
+	pCodec = NULL;
+	pFrame = NULL;
 	/*****
 	 * 2) Work environment (e.g., global data structures, file data, etc.) would
 	 *    be setup here.
 	 */
+	ret = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);   // [1]
+    if (ret != 0){
+        // error while initializing SDL
+        printf("Could not initialize SDL - %s\n.", SDL_GetError());
 
+        // exit with error
+        return -1;
+    }
+
+    av_register_all();
+    
+    ret = avformat_open_input(&pFormatCtx, argv[1], NULL, NULL);
+    if (ret < 0){
+       	printf("couldnot open file!");
+        return -1;
+    }
+
+    ret = avformat_find_stream_info(pFormatCtx, NULL);
+    if (ret < 0){
+        printf("Could not find stream information %s\n", argv[1]);
+        return -1;
+    }
+
+    av_dump_format(pFormatCtx, 0, argv[1], 0);
+
+    int i;
+    videoStream = -1;
+    for (i = 0; i < pFormatCtx->nb_streams; i++){
+        if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO){
+            videoStream = i;
+            break;
+        }
+    }
+
+    if (videoStream == -1){
+        printf("Could not find video stream.");
+        return -1;
+    }
+
+    
+    pCodec = avcodec_find_decoder(pFormatCtx->streams[videoStream]->codecpar->codec_id);
+    if (pCodec == NULL){
+        printf("Unsupported codec!\n");
+        return -1;
+    }
+
+    pCodecCtx = avcodec_alloc_context3(pCodec);
+    ret = avcodec_parameters_to_context(pCodecCtx, pFormatCtx->streams[videoStream]->codecpar);
+    if (ret != 0){
+        printf("Could not copy codec context.\n");
+        return -1;
+    }
+
+    ret = avcodec_open2(pCodecCtx, pCodec, NULL);
+    if (ret < 0){
+        printf("Could not open codec.\n");
+        return -1;
+    }
+
+    
+    pFrame = av_frame_alloc();
+    if (pFrame == NULL){
+        printf("Could not allocate frame.\n");
+        return -1;
+    }
+
+    // Create a window with the specified position, dimensions, and flags.
+    SDL_Window * screen = SDL_CreateWindow( // [2]
+                            "SDL Video Player",
+                            SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED,
+                            pCodecCtx->width/2,
+                            pCodecCtx->height/2,
+                            SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI
+    );
+
+    if (!screen){
+        // could not set video mode
+        printf("SDL: could not set video mode - exiting.\n");
+
+        // exit with Error
+        return -1;
+    }
+
+    //
+    SDL_GL_SetSwapInterval(1);
+
+    // A structure that contains a rendering state.
+    renderer = NULL;
+
+    // Use this function to create a 2D rendering context for a window.
+    renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);   // [3]
+
+    // A structure that contains an efficient, driver-specific representation
+    // of pixel data.
+    texture = NULL;
+
+    // Use this function to create a texture for a rendering context.
+    texture = SDL_CreateTexture(  // [4]
+                renderer,
+                SDL_PIXELFORMAT_YV12,
+                SDL_TEXTUREACCESS_STREAMING,
+                pCodecCtx->width,
+                pCodecCtx->height
+            );
+
+    sws_ctx = NULL;
+    pPacket = av_packet_alloc();
+    if (pPacket == NULL){
+        printf("Could not alloc packet,\n");
+        return -1;
+    }
+
+    // set up our SWSContext to convert the image data to YUV420:
+    sws_ctx = sws_getContext(
+        pCodecCtx->width,
+        pCodecCtx->height,
+        pCodecCtx->pix_fmt,
+        pCodecCtx->width,
+        pCodecCtx->height,
+        AV_PIX_FMT_YUV420P,
+        SWS_BILINEAR,
+        NULL,
+        NULL,
+        NULL
+    );
+
+    /**
+     * As we said before, we are using YV12 to display the image, and getting
+     * YUV420 data from ffmpeg.
+     */
+
+    int numBytes;
+    uint8_t * buffer = NULL;
+
+    numBytes = av_image_get_buffer_size(
+                AV_PIX_FMT_YUV420P,
+                pCodecCtx->width,
+                pCodecCtx->height,
+                32
+            );
+    buffer = (uint8_t *) av_malloc(numBytes * sizeof(uint8_t));
+
+    // used later to handle quit event
+    SDL_Event event;
+
+    pict = av_frame_alloc();
+
+    av_image_fill_arrays(
+        pict->data,
+        pict->linesize,
+        buffer,
+        AV_PIX_FMT_YUV420P,
+        pCodecCtx->width,
+        pCodecCtx->height,
+        32
+    );
+
+    int maxFramesToDecode;
+    sscanf(argv[2], "%d", &maxFramesToDecode);
 
 
 	/*****
@@ -132,9 +320,25 @@ int main(int argc, char** argv)
 	 */
 	do {
 		/* Wait until the next job is released. */
-		sleep_next_period();
+		// sleep_next_period();
 		/* Invoke job. */
-		do_exit = job();		
+		do_exit = job();
+		
+
+		// handle Ctrl + C event
+		SDL_PollEvent(&event);
+        switch(event.type){
+            case SDL_QUIT:{
+                SDL_Quit();
+                exit(0);
+            }
+            break;
+
+            default:{
+                // nothing to do
+            }
+            break;
+        }
 	} while (!do_exit);
 
 
@@ -149,37 +353,126 @@ int main(int argc, char** argv)
 	/***** 
 	 * 7) Clean up, maybe print results and stats, and exit.
 	 */
+
+	av_frame_free(&pFrame);
+    av_free(pFrame);
+    avcodec_close(pCodecCtx);
+    avformat_close_input(&pFormatCtx);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
 	return 0;
 }
 
-int i = 0;
-int job(void) 
-{
+void printHelpMenu(){
+    printf("Invalid arguments.\n\n");
+    printf("Usage: ./tutorial02 <filename> <max-frames-to-decode>\n\n");
+    printf("e.g: ./tutorial02 /home/rambodrahmani/Videos/video.mp4 200\n");
+}
+
+int job(void){
 	/* Do real-time calculation. */
-	int limit = 100000000;
-	int tm = 0;
-	for (int j =0; j < limit; j++){
-		for(int k=0; k < limit; k++){
-			for(int l=0;l<limit;l++){
-				for(int m = 0; m < limit; m++){
-					tm++;
-					tm = tm % limit;
-				}
-			}
-		}
-		//printf("%d--",i);
-	}
-	i++;
-	printf("J %d complete \n", i);
-	if(i >= 20){
-	  
-	  return 1;
-	}
-	return 0;
-	//for(i = 0; i < 500; i++){
-	//	for(j=0; j < 500; j++)
-	//		tmp = i-j;
-	//}
-	/* Don't exit. */
-	//return tmp;
+	int i = 0;
+	int ret = -1;
+    if(av_read_frame(pFormatCtx, pPacket) >= 0){
+        if (pPacket->stream_index == videoStream){
+            ret = avcodec_send_packet(pCodecCtx, pPacket);
+            if (ret < 0){
+                printf("Error sending packet for decoding.\n");
+                return -1;
+            }
+            while (ret >= 0){
+                ret = avcodec_receive_frame(pCodecCtx, pFrame);
+
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+                    break;
+                else if (ret < 0){
+                    printf("Error while decoding.\n");
+                    return -1;
+                }
+
+                // Convert the image into YUV format that SDL uses:
+                // We change the conversion format to PIX_FMT_YUV420P, and we
+                // use sws_scale just like before.
+                sws_scale(
+                    sws_ctx,
+                    (uint8_t const * const *)pFrame->data,
+                    pFrame->linesize,
+                    0,
+                    pCodecCtx->height,
+                    pict->data,
+                    pict->linesize
+                );
+
+                if (++i <= maxFramesToDecode){
+                    // get clip fps
+                    double fps = av_q2d(pFormatCtx->streams[videoStream]->r_frame_rate);
+
+                    // get clip sleep time
+                    double sleep_time = 1.0/(double)fps;
+
+                    // sleep: usleep won't work when using SDL_CreateWindow
+                    // usleep(sleep_time);
+                    // Use SDL_Delay in milliseconds to allow for cpu scheduling
+                    SDL_Delay((1000 * sleep_time) - 10);    // [5]
+
+                    // The simplest struct in SDL. It contains only four shorts. x, y which
+                    // holds the position and w, h which holds width and height.It's important
+                    // to note that 0, 0 is the upper-left corner in SDL. So a higher y-value
+                    // means lower, and the bottom-right corner will have the coordinate x + w,
+                    // y + h.
+                    SDL_Rect rect;
+                    rect.x = 0;
+                    rect.y = 0;
+                    rect.w = pCodecCtx->width;
+                    rect.h = pCodecCtx->height;
+
+                    printf(
+                        "Frame %c (%d) pts %d dts %d key_frame %d [coded_picture_number %d, display_picture_number %d, %dx%d]\n",
+                        av_get_picture_type_char(pFrame->pict_type),
+                        pCodecCtx->frame_number,
+                        pFrame->pts,
+                        pFrame->pkt_dts,
+                        pFrame->key_frame,
+                        pFrame->coded_picture_number,
+                        pFrame->display_picture_number,
+                        pCodecCtx->width,
+                        pCodecCtx->height
+                    );
+
+                    // Use this function to update a rectangle within a planar
+                    // YV12 or IYUV texture with new pixel data.
+                    SDL_UpdateYUVTexture(
+                        texture,            // the texture to update
+                        &rect,              // a pointer to the rectangle of pixels to update, or NULL to update the entire texture
+                        pict->data[0],      // the raw pixel data for the Y plane
+                        pict->linesize[0],  // the number of bytes between rows of pixel data for the Y plane
+                        pict->data[1],      // the raw pixel data for the U plane
+                        pict->linesize[1],  // the number of bytes between rows of pixel data for the U plane
+                        pict->data[2],      // the raw pixel data for the V plane
+                        pict->linesize[2]   // the number of bytes between rows of pixel data for the V plane
+                    );
+
+                    // clear the current rendering target with the drawing color
+                    SDL_RenderClear(renderer);
+
+                    // copy a portion of the texture to the current rendering target
+                    SDL_RenderCopy(
+                        renderer,   // the rendering context
+                        texture,    // the source texture
+                        NULL,       // the source SDL_Rect structure or NULL for the entire texture
+                        NULL        // the destination SDL_Rect structure or NULL for the entire rendering
+                                    // target; the texture will be stretched to fill the given rectangle
+                    );
+
+                    // update the screen with any rendering performed since the previous call
+                    SDL_RenderPresent(renderer);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        av_packet_unref(pPacket);
+    }
 }
